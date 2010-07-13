875382b980a6f169f673530508a956a84ec1826c
John Schutz <john@lisedex.com>
2010-07-13 13:49:35 -0500

    Very basic login homepage using UiBuilder and gin

--- src/com/lisedex/volinfoman/client/DefaultHomepage.java
+++ src/com/lisedex/volinfoman/client/DefaultHomepage.java
@@ -16,27 +16,60 @@
  **/
 package com.lisedex.volinfoman.client;
 
-
 import com.allen_sauer.gwt.log.client.Log;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author John Schutz <john@lisedex.com>
  *
  */
 public class DefaultHomepage extends Homepage {
-	private VerticalPanel loginPanel = new VerticalPanel();
-	private Button button = new Button();
-	
+
+	private static DefaultHomepageUiBinder uiBinder = GWT
+	.create(DefaultHomepageUiBinder.class);
+
+	interface DefaultHomepageUiBinder extends UiBinder<Widget, DefaultHomepage> {
+	}
+
+	@UiField
+	TextBox username;
+
+	@UiField
+	PasswordTextBox password;
+
+	@UiField
+	Button sendButton;
+
+	@UiField
+	Label sendStatus;
+
 	public DefaultHomepage() {
 		Log.debug("in constructor");
-		
-		button.setText("Button Text");
-		
-		loginPanel.add(button);
-		initWidget(loginPanel);
-		
+
+		initWidget(uiBinder.createAndBindUi(this));
+
+		// Can access @UiField after calling createAndBindUi
+
+		sendButton.setText("Login");
+		sendButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent e) {
+				sendStatus.setText("Sending..." + username.getText() + 
+						"/" + password.getText());
+			}
+		});
+
+		username.setFocus(true);
+		username.selectAll();
+
 		Log.debug("out constructor");
 	}
 }
